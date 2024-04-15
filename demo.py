41c83c059907dac0c18bf4528212f7130b2067b0
sobayo <holukayodeh@yahoo.com>
2024-04-15 17:28:50 -0400

    first commit

---
+++ demo.py
@@ -0,0 +1,4 @@
+from loan_status.pipeline.training_pipeline import TrainPipeline
+
+Pipeline = TrainPipeline()
+Pipeline.run_pipeline()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="loan_status",
+    version="0.0.0",
+    author="sobayo",
+    author_email="holukayodeh@gmail.com",
+    packages=find_packages()
+)
+
+
+
